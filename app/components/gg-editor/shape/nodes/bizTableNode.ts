08fdd45459715c7303aa1cb5084d00de53b26b92
weizhihua <weizhihua@flydiy.cn>
2020-06-19 11:08:30 +0800

    增加gg-editor

---
+++ app/components/gg-editor/shape/nodes/bizTableNode.ts
@@ -0,0 +1,265 @@
+import G6 from '@antv/g6';
+import merge from 'lodash/merge';
+import isArray from 'lodash/isArray';
+import {
+  CustomNode,
+  GGroup,
+  Item,
+  NodeModel
+} from 'gg-editor/lib/common/interfaces';
+import { setAnchorPointsState } from 'gg-editor';
+import { optimizeMultilineText } from '../utils';
+import { ItemState } from '../../common/constants';
+
+const TABLE_HEAD_HEIGHT = 30;
+
+const WRAPPER_BORDER_WIDTH = 2;
+const WRAPPER_HORIZONTAL_PADDING = 10;
+
+const WRAPPER_CLASS_NAME = 'node-wrapper';
+const CONTENT_CLASS_NAME = 'node-content';
+const LABEL_CLASS_NAME = 'node-label';
+const TABLE_HEAD_CLASS_NAME = 'table-head';
+const TABLE_HEAD_LABEL_CLASS_NAME = 'node-label';
+
+const bizNode: CustomNode = {
+  options: {
+    size: [120, 60],
+    wrapperStyle: {
+      fill: '#000000',
+      radius: 5
+    },
+    contentStyle: {
+      fill: '#ffffff',
+      radius: 6
+    },
+    labelStyle: {
+      fill: '#000000',
+      textAlign: 'center',
+      textBaseline: 'middle'
+    },
+    stateStyles: {
+      [ItemState.Active]: {
+        wrapperStyle: {},
+        contentStyle: {},
+        labelStyle: {}
+      } as any,
+      [ItemState.Selected]: {
+        wrapperStyle: {},
+        contentStyle: {},
+        labelStyle: {}
+      } as any
+    }
+  },
+
+  getOptions(model: NodeModel) {
+    return merge({}, this.options, this.getCustomConfig(model) || {}, model);
+  },
+
+  draw(model, group) {
+    const keyShape = this.drawWrapper(model, group);
+    this.drawTableHead(model, group);
+    this.drawTableAttr(model, group);
+    return keyShape;
+  },
+
+  drawWrapper(model: NodeModel, group: GGroup) {
+    const [width, height] = this.getSize(model);
+    const { wrapperStyle } = this.getOptions(model);
+
+    const shape = group.addShape('rect', {
+      className: WRAPPER_CLASS_NAME,
+      draggable: true,
+      attrs: {
+        x: 0,
+        y: 0,
+        width,
+        height: 30 + 3 * 27,
+        ...wrapperStyle
+      }
+    });
+
+    return shape;
+  },
+
+  drawTableHead(model: NodeModel, group: GGroup) {
+    const [width, height] = this.getSize(model);
+    const { labelStyle } = this.getOptions(model);
+
+    const tableHead = group.addShape('rect', {
+      className: TABLE_HEAD_CLASS_NAME,
+      draggable: true,
+      attrs: {
+        x: 2,
+        y: 2,
+        width: width - 4,
+        height: TABLE_HEAD_HEIGHT - 4,
+        fill: '#ffffff',
+        radius: [3, 3, 0, 0]
+      }
+    });
+    const tableHeadLabel = group.addShape('text', {
+      className: TABLE_HEAD_LABEL_CLASS_NAME,
+      draggable: true,
+      attrs: {
+        x: width / 2,
+        y: TABLE_HEAD_HEIGHT / 2,
+        width,
+        text: model.tableName,
+        fill: '#000000',
+        textAlign: 'center',
+        textBaseline: 'middle'
+      }
+    });
+
+    return tableHead;
+  },
+
+  drawTableAttr(model: NodeModel, group: GGroup) {
+    const [width, height] = this.getSize(model);
+    const { labelStyle } = this.getOptions(model);
+    const attrs = [{}, {}, {}];
+    attrs.map((attr, index) => {
+      const tableHead = group.addShape('rect', {
+        className: TABLE_HEAD_CLASS_NAME,
+        draggable: true,
+        attrs: {
+          x: 2,
+          y: 2 + TABLE_HEAD_HEIGHT - 4 + 2 + index * 27,
+          width: width - 4,
+          height: 25,
+          fill: '#ffffff'
+        }
+      });
+      // const attrShape = group.addShape('text', {
+      //   className: TABLE_HEAD_LABEL_CLASS_NAME,
+      //   draggable: true,
+      //   attrs: {
+      //     x: width / 2,
+      //     y: TABLE_HEAD_HEIGHT / 2,
+      //     width,
+      //     text: model.tableName,
+      //     fill: '#000000',
+      //     textAlign: 'center',
+      //     textBaseline: 'middle',
+      //   },
+      // });
+    });
+  },
+
+  setLabelText(model: NodeModel, group: GGroup) {
+    const shape = group.findByClassName(TABLE_HEAD_LABEL_CLASS_NAME);
+
+    if (!shape) {
+      return;
+    }
+
+    const [width] = this.getSize(model);
+    const { fontStyle, fontWeight, fontSize, fontFamily } = shape.attr();
+
+    const text = model.label as string;
+    const font = `${fontStyle} ${fontWeight} ${fontSize}px ${fontFamily}`;
+
+    shape.attr(
+      'text',
+      optimizeMultilineText(
+        text,
+        font,
+        2,
+        width - WRAPPER_HORIZONTAL_PADDING * 2
+      )
+    );
+  },
+
+  update(model, item) {
+    const group = item.getContainer();
+
+    this.setLabelText(model, group);
+  },
+
+  setState(name, value, item) {
+    const group = item.getContainer();
+    const model = item.getModel();
+    const states = item.getStates() as ItemState[];
+
+    [
+      WRAPPER_CLASS_NAME,
+      TABLE_HEAD_CLASS_NAME,
+      TABLE_HEAD_LABEL_CLASS_NAME
+    ].forEach(className => {
+      const shape = group.findByClassName(className);
+      const options = this.getOptions(model);
+
+      const shapeName = className.split('-')[1];
+
+      shape.attr({
+        ...options[`${shapeName}Style`]
+      });
+
+      states.forEach(state => {
+        if (
+          options.stateStyles[state] &&
+          options.stateStyles[state][`${shapeName}Style`]
+        ) {
+          shape.attr({
+            ...options.stateStyles[state][`${shapeName}Style`]
+          });
+        }
+      });
+    });
+
+    if (name === ItemState.Selected) {
+      const wrapperShape = group.findByClassName(WRAPPER_CLASS_NAME);
+
+      const [width, height] = this.getSize(model);
+
+      // if (value) {
+      //   wrapperShape.attr({
+      //     x: -WRAPPER_BORDER_WIDTH,
+      //     y: -WRAPPER_BORDER_WIDTH * 2,
+      //     width: width + WRAPPER_BORDER_WIDTH * 2,
+      //     height: height + WRAPPER_BORDER_WIDTH * 3,
+      //   });
+      // } else {
+      //   wrapperShape.attr({
+      //     x: 0,
+      //     y: -WRAPPER_BORDER_WIDTH * 2,
+      //     width,
+      //     height: height + WRAPPER_BORDER_WIDTH * 2,
+      //   });
+      // }
+    }
+
+    if (this.afterSetState) {
+      this.afterSetState(name, value, item);
+    }
+  },
+
+  getSize(model: NodeModel) {
+    const { size } = this.getOptions(model);
+
+    if (!isArray(size)) {
+      return [size, size];
+    }
+
+    return size;
+  },
+
+  getCustomConfig() {
+    return {};
+  },
+  afterSetState(name: string, value: string | boolean, item: Item) {
+    setAnchorPointsState.call(this, name, value, item);
+  },
+
+  getAnchorPoints() {
+    return [
+      [0.5, 0],
+      [0.5, 1],
+      [0, 0.5],
+      [1, 0.5]
+    ];
+  }
+};
+
+G6.registerNode('bizTableNode', bizNode);
